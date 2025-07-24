057263f34720d177934570f15cef9b9f499efa3b
Michel_Habre <michhabre@gmail.com>
2025-07-24 13:56:33 +0300

    Correct Hugging Face API endpoint to /models/gpt2

--- api/chat.js
+++ api/chat.js
@@ -9,7 +9,7 @@ module.exports = async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('https://api-inference.huggingface.co/pipeline/text-generation/gpt2', {
+    const response = await fetch('https://api-inference.huggingface.co/models/gpt2', {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
